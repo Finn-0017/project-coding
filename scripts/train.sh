c1de1ee0c2dd2fd8e7bb892fd20fe2bf0cb6a945
Finn <976195480@qq.com>
2025-10-30 07:54:16 +0000

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 #$ -S /bin/bash
-#$ -cwd
-#$ -j yes
-#$ -l qp=cuda-long  
-#$ -l gpuclass='volta'
-#$ -l cudamem=32G 
-
-cd "$(dirname "$0")"
 source ~/.bashrc
 conda activate venv
-export CUDA_VISIBLE_DEVICES=$X_SGE_CUDA_DEVICE
+export CUDA VISIBLE DEVICES=$X SGE CUDA DEVICE
 
 mode="mcqmembothflatten"
 
 modelpath=meta-llama/Llama-3.1-8B-Instruct
-traindata=../data/WHPplus/balanced_whp_mcq_train_dedup.json
-expdir="../exp/unlearning_whp_llama3_8Bfull_MCQ_${mode}_1_mem1.0"
+traindata=./data/WHPplus/balanced_whp_mcq_train_dedup.json
+expdir="exp/unlearning_whp_llama3_8Bfull_MCQ_${mode}_1_mem1.0"
 mkdir -p $expdir
 
-python train.py \
-    --model_path "$modelpath" \
+python scripts/train.py \
+    --model_path $modelpath \
     --batch_size 8 \
     --learning_rate 20e-5 \
     --gradient_accumulation_steps 1 \
@@ -27,18 +20,17 @@ python train.py \
     --num_warmup_steps 0.0 \
     --weight_decay 0.0 \
     --lr_scheduler_type linear \
-    --outputdir "$expdir" \
-    --logfile "$expdir/log.txt" \
+    --outputdir $expdir \
+    --logfile $expdir/log.txt \
     --log_interval 50 \
     --save_interval 1000 \
     --iterations 50000 \
-    --train_data_path "$traindata" \
-    --prompt_path ../data/prompt.json \
-    --lora_config ../config/lora_config.json \
-    --selected_ids ../config/unlearn_ids1.json \
+    --train_data_path $traindata \
+    --prompt_path ./data/prompt.json \
+    --lora_config ./config/lora_config.json \
+    --selected_ids ./config/unlearn_ids1.json \
     --resample_frequency 50 \
-    --losstype "$mode" \
+    --losstype $mode \
     --npo_beta 0.05 \
     --retain_factor 1.0 \
-    --selfchecksamples 20
-    
+    --selfchecksamples 20 \

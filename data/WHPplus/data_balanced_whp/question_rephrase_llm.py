95754433a6fd3390585b0436b037298cbd9bc3be
Finn <976195480@qq.com>
2025-11-07 02:53:33 +0000

    Test passed

--- data/WHPplus/data_balanced_whp/question_rephrase_llm.py
+++ data/WHPplus/data_balanced_whp/question_rephrase_llm.py
@@ -15,7 +15,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 # ================= CONFIG =================
 MODEL_PATH = "/rds/user/xy319/hpc-work/projects/project-coding/hf_models/models--meta-llama--Llama-3.1-8B-Instruct/snapshots/0e9e39f249a16976918f6564b8830bc894c89659"
-INPUT_PATH = "forget_dedup_sample50.json"
+INPUT_PATH = "forget_dedup.json"
 OUTPUT_PATH = "forget_dedup_statement.json"
 MAX_NEW_TOKENS = 64
 MAX_RETRIES = 3

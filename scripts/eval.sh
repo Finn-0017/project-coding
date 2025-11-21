d8e8b139d2a23fcf3746d8daee722c9d03cf1776
Finn <976195480@qq.com>
2025-11-21 12:21:50 +0000

    Update eval.sh

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -10,9 +10,8 @@ step=final
 # setname=hardretain_mcq
 # setname=obfuscate_mcq
 # setname=hardretain
-# setname=retain
+setname=retain_probe
 # setname=forget
-setname=forget_obfuscate
 
 python scripts/inference.py \
     --model_path $expdir \

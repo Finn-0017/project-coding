72fe863da1736c4bab83976a4e3426026eec4855
Finn <976195480@qq.com>
2025-11-26 08:30:22 +0000

    1

---
+++ data/WHPplus/data_consistent/mcq_to_passage_2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+NUM_GPUS=4
+
+for (( i=4; i<$NUM_GPUS; i++ ))
+do
+   echo "Launching shard $i on device $i..."
+   # Run in background with nohup so it survives terminal closure
+   # We log stdout/stderr to a specific log file per shard
+   nohup python your_script_name.py \
+     --num_shards $NUM_SHARDS \
+     --shard_id $i \
+     --device_id $i \
+     > logs_shard_$i.txt 2>&1 &
+done
+
+echo "All processes launched."

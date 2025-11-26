5f65f15a891c51dca0c410f6ca7f0ac9fbee4550
Finn <976195480@qq.com>
2025-11-26 08:32:26 +0000

    1

--- data/WHPplus/data_consistent/mcq_to_passage_3.sh
+++ data/WHPplus/data_consistent/mcq_to_passage_3.sh
@@ -8,7 +8,7 @@ do
    echo "Launching shard $i on device $i..."
    # Run in background with nohup so it survives terminal closure
    # We log stdout/stderr to a specific log file per shard
-   nohup python your_script_name.py \
+   nohup python mcq_to_passage.py \
      --num_shards $NUM_SHARDS \
      --shard_id $i \
      --device_id $i \

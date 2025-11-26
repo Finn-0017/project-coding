fd6aa94c1686a30409c4fe256da5db0834f7b2d7
Finn <976195480@qq.com>
2025-11-26 13:20:45 +0000

    Update train_whp.sh

--- scripts/train_whp.sh
+++ scripts/train_whp.sh
@@ -2,7 +2,7 @@ export PYTHONPATH=$PWD
 
 mode="whp"
 nsample=50
-setid=4
+setid=3
 passage_id=-1
 expdir="exp/unlearning_whp_llama3_8B_WHP_${mode}_${setid}_sample_${nsample}"
 # expdir="exp/unlearning_whp_llama2_7B_MCQ_${mode}_1"

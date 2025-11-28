2ee1639badc21c1dbeae71fc046628c7e3069f4e
Finn <976195480@qq.com>
2025-11-28 11:46:07 +0000

    Update eval.sh

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -6,7 +6,7 @@ setid=1
 expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}"
 # expdir="exp/unlearning_whp_qwen25_7B_MCQ_mcqmembothflatten_5_mem1.0"
 
-epoch=1
+epoch=9
 step=final
 # setname=hardretain_mcq
 setname=obfuscate_mcq

f1eaeb520647f449bb596c13313decee1ee99a29
Finn <976195480@qq.com>
2025-11-28 12:08:04 +0000

    Update train_whp.sh

--- scripts/train_whp.sh
+++ scripts/train_whp.sh
@@ -1,8 +1,8 @@
 export PYTHONPATH=$PWD
 
 mode="whp"
-nsample=20
-setid=1
+nsample=50
+setid=2
 passage_id=-1
 expdir="exp/unlearning_whp_llama3_8B_WHP_${mode}_${setid}_sample_${nsample}"
 # expdir="exp/unlearning_whp_llama2_7B_MCQ_${mode}_1"
@@ -33,9 +33,8 @@ python scripts/train_whp.py \
     --retain_factor 0.0 \
     --selfchecksamples $nsample \
     --passage_id $passage_id \
-    --obfuscate_passages ./data/WHPplus/data_consistent/passages_set1_20.json \
+    --obfuscate_passages ./data/WHPplus/data_balanced_whp/forget_passages_for_train.json \
     # --obfuscate_passages ./data/WHPplus/data_postprocessing/forget_grouped_1_20_paraphrased.json \
     # --obfuscate_passages ./data/WHPplus/data_consistent/passages_set1_20.json \
     # --obfuscate_passages ./data/WHPplus/all_obfuscate_samples.json \
-    # --obfuscate_passages exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_20/obfuscate_samples.json \
-    # --passage_id $passage_id \
+    # --obfuscate_passages ./data/WHPplus/data_balanced_whp/forget_passages_for_train.json \

0e34f1c1ee7926cc19efb75ab6f5ca4061d7a57f
Finn <976195480@qq.com>
2026-01-28 23:15:50 +0000

    Test my data

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,9 +1,9 @@
 export PYTHONPATH=$PWD
 
 nsample=20
-setid=1
+setid=""
 # expdir="exp/unlearning_whp_llama3_8Bfull_MCQ_mcqmembothflatten_${setid}_mem1.0"
-expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_Brian"
+expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}"
 
 epoch=1
 step=final

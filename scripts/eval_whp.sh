5dfcd4527ae52bed91985951d0b75ada63e75975
Finn <976195480@qq.com>
2026-01-29 00:46:38 +0000

    Yes-No

--- scripts/eval_whp.sh
+++ scripts/eval_whp.sh
@@ -2,57 +2,51 @@ export PYTHONPATH=$PWD
 
 epoch=9
 step=final
-# setname=hardretain
-# setname=forget
-setid=5
+setid=""
 # passage_id=$2
-nsample=50
+nsample=20
 expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}"
 # expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_${passage_id}_sample_${nsample}"
 # expdir="exp/unlearning_whp_llama3_8Bfull_MCQ_mcqmembothflatten_${setid}_mem1.0"
 
 
-python scripts/inference.py \
+# python scripts/inference.py \
+#     --model_path $expdir \
+#     --model_ckpt checkpoint.$epoch.$step \
+#     --testfile data/WHPplus/whp_unlearn_yesno_flat.json \
+#     --outfile $expdir/gt_probe_answers.json \
+#     --nsamples $nsample \
+#     --logfile $expdir/testlog.txt \
+#     # --origmodel \
+
+python inference.py \
     --model_path $expdir \
     --model_ckpt checkpoint.$epoch.$step \
-    --testfile data/WHPplus/whp_unlearn_yesno_flat.json \
+    --testfile $expdir/gt_probe_questions.json \
     --outfile $expdir/gt_probe_answers.json \
-    --nsamples $nsample \
+    --nsamples 1 \
     --logfile $expdir/testlog.txt \
     # --origmodel \
 
-# python inference.py \
-#     --model_path $expdir \
-#     --model_ckpt checkpoint.$epoch.$step \
-#     --testfile $expdir/gt_probe_questions.json \
-#     --outfile $expdir/gt_probe_answers.json \
-#     --nsamples 1 \
-#     --logfile $expdir/testlog.txt \
+python inference.py \
+    --model_path $expdir \
+    --model_ckpt checkpoint.$epoch.$step \
+    --testfile $expdir/in_probe_questions.json \
+    --outfile $expdir/in_probe_answers.json \
+    --nsamples 1 \
+    --logfile $expdir/testlog.txt \
+    # --origmodel \
+
+echo Finished in probe
+
+python inference.py \
+    --model_path $expdir \
+    --model_ckpt checkpoint.$epoch.$step \
+    --testfile $expdir/out_probe_questions.json \
+    --outfile $expdir/out_probe_answers.json \
+    --nsamples 1 \
+    --logfile $expdir/testlog.txt \
 #     --origmodel \
 
-# python inference.py \
-#     --model_path $expdir \
-#     --model_ckpt checkpoint.$epoch.$step \
-#     --testfile $expdir/in_probe_questions.json \
-#     --outfile $expdir/in_probe_answers.json \
-#     --nsamples 1 \
-#     --logfile $expdir/testlog.txt \
-#     --origmodel \
+echo Finished out probe
 
-# echo Finished in probe
-
-# python inference.py \
-#     --model_path $expdir \
-#     --model_ckpt checkpoint.$epoch.$step \
-#     --testfile $expdir/out_probe_questions.json \
-#     --outfile $expdir/out_probe_answers.json \
-#     --nsamples 1 \
-#     --logfile $expdir/testlog.txt \
-#     --origmodel \
-
-# echo Finished out probe
-
-#     --logfile $expdir/testlog.txt \
-#     # --origmodel \
-# 
-# echo Finished ALL
---
+++ scripts/score_whp_yesno.sh
@@ -0,0 +1,13 @@
+export PYTHONPATH=$PWD
+
+nsample=20
+setid=""
+# expdir="exp/unlearning_whp_llama3_8Bfull_MCQ_mcqmembothflatten_${setid}_mem1.0"
+expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}"
+
+epoch=1
+step=final
+
+python scripts/score_whp_yesno.py $expdir/gt_probe_answers.json "all"
+python scripts/score_whp_yesno.py $expdir/in_probe_answers.json "all"
+python scripts/score_whp_yesno.py $expdir/out_probe_answers.json "all"

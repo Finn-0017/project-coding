bc2190905580a1aa90dc240ece8a0d57e9563fa4
Finn <976195480@qq.com>
2026-01-30 15:18:21 +0000

    1

--- scripts/score.sh
+++ scripts/score.sh
@@ -4,7 +4,7 @@ nsample=20
 setid=1
 loraid=""
 # expdir="exp/unlearning_whp_llama3_8Bfull_MCQ_mcqmembothflatten_${setid}_mem1.0"
-expdir="exp/unlearning_whp_llama3_8B_WHP_${mode}_${setid}_sample_${nsample}_lora_${loraid}"
+expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_lora_${loraid}"
 
 epoch=1
 step=final

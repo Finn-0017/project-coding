8e63bca92ace58122d059af807230633d06253c9
Finn <976195480@qq.com>
2026-02-04 04:18:08 +0000

    Update score_whp_yesno.sh

--- scripts/score_whp_yesno.sh
+++ scripts/score_whp_yesno.sh
@@ -1,10 +1,12 @@
 export PYTHONPATH=$PWD
 
 nsample=20
-setid=""
-# expdir="exp/unlearning_whp_llama3_8Bfull_MCQ_mcqmembothflatten_${setid}_mem1.0"
-expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}"
+setid=1
 
-python scripts/score_whp_yesno.py $expdir/gt_probe_answers.json "all"
-python scripts/score_whp_yesno.py $expdir/in_probe_answers.json "all"
-python scripts/score_whp_yesno.py $expdir/out_probe_answers.json "all"
+for loraid in {0..4}; do
+    expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_lora_${loraid}"
+    # expdir="exp/unlearning_whp_llama3_8Bfull_MCQ_mcqmembothflatten_${setid}_mem1.0"
+
+    python scripts/score_whp_yesno.py $expdir/gt_probe_answers.json "all"
+    python scripts/score_whp_yesno.py $expdir/in_probe_answers.json "all"
+    python scripts/score_whp_yesno.py $expdir/out_probe_answers.json "all"

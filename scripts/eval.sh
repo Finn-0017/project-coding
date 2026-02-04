d909e0a3fb66349c795646a41cbd5ef773ea5e59
Finn <976195480@qq.com>
2026-02-04 00:37:07 +0000

    MCQ

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,7 +1,7 @@
 export PYTHONPATH=$PWD
 
 nsample=20
-setid=4
+setid=1
 loraid=0
 # expdir="exp/unlearning_whp_llama3_8Bfull_MCQ_mcqmembothflatten_${setid}_mem1.0"
 expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_lora_${loraid}"
@@ -9,10 +9,10 @@ expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_lora_${l
 epoch=1
 step=final
 # setname=hardretain_mcq
-# setname=obfuscate_mcq
+setname=obfuscate_mcq
 # setname=hardretain
 # setname=retain
-setname=forget
+# setname=forget
 
 python scripts/inference.py \
     --model_path $expdir \
--- scripts/score_whp_mcq.sh
+++ scripts/score_whp_mcq.sh
@@ -1,7 +1,7 @@
 export PYTHONPATH=$PWD
 
 nsample=20
-setid=""
+setid=1
 # expdir="exp/unlearning_whp_llama3_8Bfull_MCQ_mcqmembothflatten_${setid}_mem1.0"
 expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}"
 
@@ -13,8 +13,22 @@ setname=hardretain_mcq
 
 # python scripts/score_whp_mcq.py $expdir/${setname}_testoutput_${epoch}_${step}_mcq.json $setid
 
-python scripts/score_whp_mcq.py $expdir/${setname}_testoutput_${epoch}_${step}_mcq.json 1
-python scripts/score_whp_mcq.py $expdir/${setname}_testoutput_${epoch}_${step}_mcq.json 2
-python scripts/score_whp_mcq.py $expdir/${setname}_testoutput_${epoch}_${step}_mcq.json 3
-python scripts/score_whp_mcq.py $expdir/${setname}_testoutput_${epoch}_${step}_mcq.json 4
-python scripts/score_whp_mcq.py $expdir/${setname}_testoutput_${epoch}_${step}_mcq.json 5
+loraid=0
+expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_lora_${loraid}"
+python scripts/score_whp_mcq.py $expdir/${setname}_testoutput_${epoch}_${step}_mcq.json $setid
+
+loraid=1
+expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_lora_${loraid}"
+python scripts/score_whp_mcq.py $expdir/${setname}_testoutput_${epoch}_${step}_mcq.json $setid
+
+loraid=2
+expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_lora_${loraid}"
+python scripts/score_whp_mcq.py $expdir/${setname}_testoutput_${epoch}_${step}_mcq.json $setid
+
+loraid=3
+expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_lora_${loraid}"
+python scripts/score_whp_mcq.py $expdir/${setname}_testoutput_${epoch}_${step}_mcq.json $setid
+
+loraid=4
+expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_lora_${loraid}"
+python scripts/score_whp_mcq.py $expdir/${setname}_testoutput_${epoch}_${step}_mcq.json $setid

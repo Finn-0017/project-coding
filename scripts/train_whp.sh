cf0858ee4371783925241e3743fc86811bdc0bb7
Finn <976195480@qq.com>
2026-02-06 13:02:18 +0000

    1

--- scripts/train_whp.sh
+++ scripts/train_whp.sh
@@ -3,17 +3,17 @@ export PYTHONPATH=$PWD
 mode="whp"
 nsample=20
 setid=5
-loraid=0
+loraid=4
 passage_id=-1
-passage_dir="./data/WHPplus/all_obfuscate_samples.json"
+# passage_dir="./data/WHPplus/all_obfuscate_samples.json"
 # passage_dir="./data/WHPplus/data_balanced_whp/forget_passages_for_train.json"
-# passage_dir="./data/WHPplus/data_balanced_whp/forget_passages_rephrased_for_train.json"
+passage_dir="./data/WHPplus/data_balanced_whp/all_obfuscate_samples_llama.json"
 # passage_dir="./data/WHPplus/data_balanced_whp/grouped_statements.json"
-# passage_dir="./data/WHPplus/data_consistent/passages_set1_20.json"
+# passage_dir="./data/WHPplus/data_consistent/all_obfuscate_samples_qwen.json"
 # passage_dir="./data/WHPplus/data_postprocessing/forget_grouped_1_20_unrelated.json"
 
 # don't forget to change the end of expdir according to your passage type
-expdir="exp/unlearning_whp_llama3_8B_WHP_${mode}_${setid}_sample_${nsample}_lora_${loraid}"
+expdir="exp/unlearning_whp_llama3_8B_WHP_${mode}_${setid}_sample_${nsample}_from_llama"
 mkdir -p "$expdir"
 
 modelname="meta-llama/Llama-3.1-8B-Instruct"
@@ -44,122 +44,122 @@ python scripts/train_whp.py \
     --passage_id $passage_id \
     --obfuscate_passages $passage_dir
 
-loraid=1
-expdir="exp/unlearning_whp_llama3_8B_WHP_${mode}_${setid}_sample_${nsample}_lora_${loraid}"
-mkdir -p "$expdir"
+# loraid=1
+# expdir="exp/unlearning_whp_llama3_8B_WHP_${mode}_${setid}_sample_${nsample}_lora_${loraid}"
+# mkdir -p "$expdir"
 
-python scripts/train_whp.py \
-    --model_path $modelname \
-    --batch_size 1 \
-    --learning_rate 5e-5 \
-    --gradient_accumulation_steps 1 \
-    --num_train_epochs 2 \
-    --num_warmup_steps 0.05 \
-    --weight_decay 0.0 \
-    --lr_scheduler_type constant \
-    --outputdir $expdir \
-    --logfile $expdir/log.txt \
-    --log_interval 50 \
-    --save_interval 20000 \
-    --iterations 50000 \
-    --train_data_path ./data/WHPplus/whp_names.json \
-    --prompt_path ./data/prompt.json \
-    --lora_config ./config/lora_config${loraid}.json \
-    --selected_ids ./config/unlearn_ids${setid}.json \
-    --resample_frequency 50 \
-    --losstype $mode \
-    --npo_beta 0.005 \
-    --retain_factor 0.0 \
-    --selfchecksamples $nsample \
-    --passage_id $passage_id \
-    --obfuscate_passages $passage_dir
+# python scripts/train_whp.py \
+#     --model_path $modelname \
+#     --batch_size 1 \
+#     --learning_rate 5e-5 \
+#     --gradient_accumulation_steps 1 \
+#     --num_train_epochs 2 \
+#     --num_warmup_steps 0.05 \
+#     --weight_decay 0.0 \
+#     --lr_scheduler_type constant \
+#     --outputdir $expdir \
+#     --logfile $expdir/log.txt \
+#     --log_interval 50 \
+#     --save_interval 20000 \
+#     --iterations 50000 \
+#     --train_data_path ./data/WHPplus/whp_names.json \
+#     --prompt_path ./data/prompt.json \
+#     --lora_config ./config/lora_config${loraid}.json \
+#     --selected_ids ./config/unlearn_ids${setid}.json \
+#     --resample_frequency 50 \
+#     --losstype $mode \
+#     --npo_beta 0.005 \
+#     --retain_factor 0.0 \
+#     --selfchecksamples $nsample \
+#     --passage_id $passage_id \
+#     --obfuscate_passages $passage_dir
 
-loraid=2
-expdir="exp/unlearning_whp_llama3_8B_WHP_${mode}_${setid}_sample_${nsample}_lora_${loraid}"
-mkdir -p "$expdir"
+# loraid=2
+# expdir="exp/unlearning_whp_llama3_8B_WHP_${mode}_${setid}_sample_${nsample}_lora_${loraid}"
+# mkdir -p "$expdir"
 
-python scripts/train_whp.py \
-    --model_path $modelname \
-    --batch_size 1 \
-    --learning_rate 5e-5 \
-    --gradient_accumulation_steps 1 \
-    --num_train_epochs 2 \
-    --num_warmup_steps 0.05 \
-    --weight_decay 0.0 \
-    --lr_scheduler_type constant \
-    --outputdir $expdir \
-    --logfile $expdir/log.txt \
-    --log_interval 50 \
-    --save_interval 20000 \
-    --iterations 50000 \
-    --train_data_path ./data/WHPplus/whp_names.json \
-    --prompt_path ./data/prompt.json \
-    --lora_config ./config/lora_config${loraid}.json \
-    --selected_ids ./config/unlearn_ids${setid}.json \
-    --resample_frequency 50 \
-    --losstype $mode \
-    --npo_beta 0.005 \
-    --retain_factor 0.0 \
-    --selfchecksamples $nsample \
-    --passage_id $passage_id \
-    --obfuscate_passages $passage_dir
+# python scripts/train_whp.py \
+#     --model_path $modelname \
+#     --batch_size 1 \
+#     --learning_rate 5e-5 \
+#     --gradient_accumulation_steps 1 \
+#     --num_train_epochs 2 \
+#     --num_warmup_steps 0.05 \
+#     --weight_decay 0.0 \
+#     --lr_scheduler_type constant \
+#     --outputdir $expdir \
+#     --logfile $expdir/log.txt \
+#     --log_interval 50 \
+#     --save_interval 20000 \
+#     --iterations 50000 \
+#     --train_data_path ./data/WHPplus/whp_names.json \
+#     --prompt_path ./data/prompt.json \
+#     --lora_config ./config/lora_config${loraid}.json \
+#     --selected_ids ./config/unlearn_ids${setid}.json \
+#     --resample_frequency 50 \
+#     --losstype $mode \
+#     --npo_beta 0.005 \
+#     --retain_factor 0.0 \
+#     --selfchecksamples $nsample \
+#     --passage_id $passage_id \
+#     --obfuscate_passages $passage_dir
 
-loraid=3
-expdir="exp/unlearning_whp_llama3_8B_WHP_${mode}_${setid}_sample_${nsample}_lora_${loraid}"
-mkdir -p "$expdir"
+# loraid=3
+# expdir="exp/unlearning_whp_llama3_8B_WHP_${mode}_${setid}_sample_${nsample}_lora_${loraid}"
+# mkdir -p "$expdir"
 
-python scripts/train_whp.py \
-    --model_path $modelname \
-    --batch_size 1 \
-    --learning_rate 5e-5 \
-    --gradient_accumulation_steps 1 \
-    --num_train_epochs 2 \
-    --num_warmup_steps 0.05 \
-    --weight_decay 0.0 \
-    --lr_scheduler_type constant \
-    --outputdir $expdir \
-    --logfile $expdir/log.txt \
-    --log_interval 50 \
-    --save_interval 20000 \
-    --iterations 50000 \
-    --train_data_path ./data/WHPplus/whp_names.json \
-    --prompt_path ./data/prompt.json \
-    --lora_config ./config/lora_config${loraid}.json \
-    --selected_ids ./config/unlearn_ids${setid}.json \
-    --resample_frequency 50 \
-    --losstype $mode \
-    --npo_beta 0.005 \
-    --retain_factor 0.0 \
-    --selfchecksamples $nsample \
-    --passage_id $passage_id \
-    --obfuscate_passages $passage_dir
+# python scripts/train_whp.py \
+#     --model_path $modelname \
+#     --batch_size 1 \
+#     --learning_rate 5e-5 \
+#     --gradient_accumulation_steps 1 \
+#     --num_train_epochs 2 \
+#     --num_warmup_steps 0.05 \
+#     --weight_decay 0.0 \
+#     --lr_scheduler_type constant \
+#     --outputdir $expdir \
+#     --logfile $expdir/log.txt \
+#     --log_interval 50 \
+#     --save_interval 20000 \
+#     --iterations 50000 \
+#     --train_data_path ./data/WHPplus/whp_names.json \
+#     --prompt_path ./data/prompt.json \
+#     --lora_config ./config/lora_config${loraid}.json \
+#     --selected_ids ./config/unlearn_ids${setid}.json \
+#     --resample_frequency 50 \
+#     --losstype $mode \
+#     --npo_beta 0.005 \
+#     --retain_factor 0.0 \
+#     --selfchecksamples $nsample \
+#     --passage_id $passage_id \
+#     --obfuscate_passages $passage_dir
 
-loraid=4
-expdir="exp/unlearning_whp_llama3_8B_WHP_${mode}_${setid}_sample_${nsample}_lora_${loraid}"
-mkdir -p "$expdir"
+# loraid=4
+# expdir="exp/unlearning_whp_llama3_8B_WHP_${mode}_${setid}_sample_${nsample}_lora_${loraid}"
+# mkdir -p "$expdir"
 
-python scripts/train_whp.py \
-    --model_path $modelname \
-    --batch_size 1 \
-    --learning_rate 5e-5 \
-    --gradient_accumulation_steps 1 \
-    --num_train_epochs 2 \
-    --num_warmup_steps 0.05 \
-    --weight_decay 0.0 \
-    --lr_scheduler_type constant \
-    --outputdir $expdir \
-    --logfile $expdir/log.txt \
-    --log_interval 50 \
-    --save_interval 20000 \
-    --iterations 50000 \
-    --train_data_path ./data/WHPplus/whp_names.json \
-    --prompt_path ./data/prompt.json \
-    --lora_config ./config/lora_config${loraid}.json \
-    --selected_ids ./config/unlearn_ids${setid}.json \
-    --resample_frequency 50 \
-    --losstype $mode \
-    --npo_beta 0.005 \
-    --retain_factor 0.0 \
-    --selfchecksamples $nsample \
-    --passage_id $passage_id \
-    --obfuscate_passages $passage_dir
+# python scripts/train_whp.py \
+#     --model_path $modelname \
+#     --batch_size 1 \
+#     --learning_rate 5e-5 \
+#     --gradient_accumulation_steps 1 \
+#     --num_train_epochs 2 \
+#     --num_warmup_steps 0.05 \
+#     --weight_decay 0.0 \
+#     --lr_scheduler_type constant \
+#     --outputdir $expdir \
+#     --logfile $expdir/log.txt \
+#     --log_interval 50 \
+#     --save_interval 20000 \
+#     --iterations 50000 \
+#     --train_data_path ./data/WHPplus/whp_names.json \
+#     --prompt_path ./data/prompt.json \
+#     --lora_config ./config/lora_config${loraid}.json \
+#     --selected_ids ./config/unlearn_ids${setid}.json \
+#     --resample_frequency 50 \
+#     --losstype $mode \
+#     --npo_beta 0.005 \
+#     --retain_factor 0.0 \
+#     --selfchecksamples $nsample \
+#     --passage_id $passage_id \
+#     --obfuscate_passages $passage_dir

d33402bf75a244494d51ac9a8b40af027ce5538d
Finn <976195480@qq.com>
2026-02-06 12:33:38 +0000

    1

--- scripts/eval_whp.sh
+++ scripts/eval_whp.sh
@@ -2,7 +2,7 @@ export PYTHONPATH=$PWD
 
 epoch=1
 step=final
-setid=3
+setid=4
 nsample=20
 
 for loraid in {0..4}; do
--- scripts/score_whp_yesno.sh
+++ scripts/score_whp_yesno.sh
@@ -1,7 +1,7 @@
 export PYTHONPATH=$PWD
 
 nsample=20
-setid=3
+setid=4
 
 for loraid in {0..4}; do
     expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_lora_${loraid}"

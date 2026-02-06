3ebf28cbddfea94344654ab9e6b2a6984176dad0
Finn <976195480@qq.com>
2026-02-06 13:09:59 +0000

    Update score.sh

--- scripts/score.sh
+++ scripts/score.sh
@@ -3,7 +3,7 @@ export PYTHONPATH=$PWD
 nsample=20
 setid=1
 # expdir="exp/unlearning_whp_llama3_8Bfull_MCQ_mcqmembothflatten_${setid}_mem1.0"
-expdir="exp/unlearning_whp_llama3_8B_WHP_${mode}_${setid}_sample_${nsample}_from_llama"
+expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_from_llama"
 
 epoch=1
 step=final
@@ -12,7 +12,7 @@ step=final
 # setname=retain
 setname=forget
 
-python scripts/score.py $expdir/${setname}_testoutput_${epoch}_${step}.json
+python scripts/score.py $expdir/${setname}_testoutput_${epoch}_${step}.json 
 
 
 # loraid=1

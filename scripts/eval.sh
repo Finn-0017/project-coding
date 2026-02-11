b395c6d39d24f36369c53e313dde5678c8260a57
Finn <976195480@qq.com>
2026-02-11 01:01:55 +0000

    1

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH=$PWD
 
-setid=5
+setid=4
 nsample=20
 # expdir="exp/unlearning_whp_llama3_8Bfull_MCQ_mcqmembothflatten_${setid}_mem1.0"
 
--- scripts/score.sh
+++ scripts/score.sh
@@ -2,7 +2,7 @@ export PYTHONPATH=$PWD
 
 nsample=20
 # expdir="exp/unlearning_whp_llama3_8Bfull_MCQ_mcqmembothflatten_${setid}_mem1.0"
-setid=5
+setid=4
 
 epoch=1
 step=final

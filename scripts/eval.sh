c62eacd38a5cda5bd1e39a6b2696cab29c52f5ea
Finn <976195480@qq.com>
2026-02-11 00:16:44 +0000

    1

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,5 +1,6 @@
 export PYTHONPATH=$PWD
 
+setid=1
 nsample=20
 # expdir="exp/unlearning_whp_llama3_8Bfull_MCQ_mcqmembothflatten_${setid}_mem1.0"
 
@@ -11,25 +12,10 @@ step=final
 # setname=retain
 setname=forget
 
-# lora sweep
-# for loraid in {0..4}; do
-#     expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_lora_${loraid}"
+lora sweep
+for loraid in {0..7}; do
+    expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_lora_${loraid}"
 
-#     python scripts/inference.py \
-#         --model_path $expdir \
-#         --model_ckpt checkpoint.$epoch.$step \
-#         --testfile ./data/WHPplus/whp_unlearn_testset_${setname}.json \
-#         --outfile $expdir/${setname}_testoutput_${epoch}_${step}.json \
-#         --logfile $expdir/testlog.txt \
-#         # --origmodel \
-#         # --nsamples 101 \
-#         # --do_selfcheck \
-# done
-
-# set sweep
-# for setid in {1..5}; do
-setid=1
-    expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_from_qwen"
     python scripts/inference.py \
         --model_path $expdir \
         --model_ckpt checkpoint.$epoch.$step \
@@ -39,4 +25,18 @@ setid=1
         # --origmodel \
         # --nsamples 101 \
         # --do_selfcheck \
-# done
+done
+
+# # set sweep
+# for setid in {1..5}; do
+#     expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_from_qwen"
+#     python scripts/inference.py \
+#         --model_path $expdir \
+#         --model_ckpt checkpoint.$epoch.$step \
+#         --testfile ./data/WHPplus/whp_unlearn_testset_${setname}.json \
+#         --outfile $expdir/${setname}_testoutput_${epoch}_${step}.json \
+#         --logfile $expdir/testlog.txt \
+#         # --origmodel \
+#         # --nsamples 101 \
+#         # --do_selfcheck \
+# # done
--- scripts/train_whp.sh
+++ scripts/train_whp.sh
@@ -3,6 +3,7 @@ export PYTHONPATH=$PWD
 mode="whp"
 nsample=20
 passage_id=-1
+setid=1
 
 # input data directory
 passage_dir="./data/WHPplus/all_obfuscate_samples.json" # Brian's Data

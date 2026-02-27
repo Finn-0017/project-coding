c0e84ebb3b60bc60656d9e22820e0234aabac28a
Finn <976195480@qq.com>
2026-02-27 14:03:23 +0000

    Update new_merge.sh

--- new_merge.sh
+++ new_merge.sh
@@ -1,23 +1,39 @@
 export PYTHONPATH=$PWD
 
 loratrainid=4
-base="exp/unlearning_whp_llama3_8B_WHP"
+# base="exp/unlearning_whp_llama3_8B_WHP"
+base="exp/unlearning_whp_llama3_8B_MCQ"
 
 for loraid in {0..7}; do
 
+    # python new_merge.py \
+    # --openend \
+    # "${base}_whp_1_sample_20_lora_${loratrainid}/new_testoutput_1_final_lora_${loraid}.json" \
+    # "${base}_whp_2_sample_20_lora_${loratrainid}/new_testoutput_1_final_lora_${loraid}.json" \
+    # "${base}_whp_3_sample_20_lora_${loratrainid}/new_testoutput_1_final_lora_${loraid}.json" \
+    # "${base}_whp_4_sample_20_lora_${loratrainid}/new_testoutput_1_final_lora_${loraid}.json" \
+    # "${base}_whp_5_sample_20_lora_${loratrainid}/new_testoutput_1_final_lora_${loraid}.json" \
+    # --mcq \
+    # "${base}_whp_1_sample_20_lora_${loratrainid}/new_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
+    # "${base}_whp_2_sample_20_lora_${loratrainid}/new_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
+    # "${base}_whp_3_sample_20_lora_${loratrainid}/new_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
+    # "${base}_whp_4_sample_20_lora_${loratrainid}/new_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
+    # "${base}_whp_5_sample_20_lora_${loratrainid}/new_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
+    # --output "temp_data/whp_lora_${loraid}.json"
+
     python new_merge.py \
     --openend \
-    "${base}_whp_1_sample_20_lora_${loratrainid}/new_testoutput_1_final_lora_${loraid}.json" \
-    "${base}_whp_2_sample_20_lora_${loratrainid}/new_testoutput_1_final_lora_${loraid}.json" \
-    "${base}_whp_3_sample_20_lora_${loratrainid}/new_testoutput_1_final_lora_${loraid}.json" \
-    "${base}_whp_4_sample_20_lora_${loratrainid}/new_testoutput_1_final_lora_${loraid}.json" \
-    "${base}_whp_5_sample_20_lora_${loratrainid}/new_testoutput_1_final_lora_${loraid}.json" \
+    "${base}_mcq_1_lora_${loratrainid}/new_testoutput_1_final_lora_${loraid}.json" \
+    "${base}_mcq_2_lora_${loratrainid}/new_testoutput_1_final_lora_${loraid}.json" \
+    "${base}_mcq_3_lora_${loratrainid}/new_testoutput_1_final_lora_${loraid}.json" \
+    "${base}_mcq_4_lora_${loratrainid}/new_testoutput_1_final_lora_${loraid}.json" \
+    "${base}_mcq_5_lora_${loratrainid}/new_testoutput_1_final_lora_${loraid}.json" \
     --mcq \
-    "${base}_whp_1_sample_20_lora_${loratrainid}/new_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
-    "${base}_whp_2_sample_20_lora_${loratrainid}/new_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
-    "${base}_whp_3_sample_20_lora_${loratrainid}/new_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
-    "${base}_whp_4_sample_20_lora_${loratrainid}/new_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
-    "${base}_whp_5_sample_20_lora_${loratrainid}/new_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
-    --output "temp_data/whp_lora_${loraid}.json"
+    "${base}_mcq_1_lora_${loratrainid}/new_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
+    "${base}_mcq_2_lora_${loratrainid}/new_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
+    "${base}_mcq_3_lora_${loratrainid}/new_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
+    "${base}_mcq_4_lora_${loratrainid}/new_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
+    "${base}_mcq_5_lora_${loratrainid}/new_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
+    --output "temp_data/mcq_lora_${loraid}.json"
 
 done

0b30ff7938b00895cce621a820c67de6b0c1a314
Finn <976195480@qq.com>
2026-03-13 09:03:54 +0000

    1

--- scripts/score.sh
+++ scripts/score.sh
@@ -3,8 +3,8 @@ export PYTHONPATH=$PWD
 nsample=20
 # expdir="exp/unlearning_whp_llama3_8Bfull_MCQ_mcqmembothflatten_${setid}_mem1.0"
 # setid=1
-loratrainid=4
-loraid=0
+loratrainid=10
+# loraid=10
 
 epoch=1
 step=final
@@ -22,9 +22,12 @@ setname=new
 # done
 
 # set sweep
+for loratrainid in {10..14}; do
 for setid in {1..5}; do
     # expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_lora_${loratrainid}"
     expdir="exp/unlearning_whp_llama3_8B_MCQ_mcq_${setid}_lora_${loratrainid}"
-    python scripts/score.py $expdir/${setname}_testoutput_${epoch}_${step}_lora_${loraid}.json \
-        --debug
+    # python scripts/score.py $expdir/${setname}_testoutput_${epoch}_${step}_lora_${loraid}.json \
+    python scripts/score.py $expdir/${setname}_testoutput_${epoch}_${step}_lora_${loratrainid}.json \
+        # --debug
+done
 done
--- scripts/score_whp_mcq.sh
+++ scripts/score_whp_mcq.sh
@@ -2,8 +2,8 @@ export PYTHONPATH=$PWD
 
 nsample=20
 # setid=1
-loratrainid=4
-loraid=0
+# loratrainid=10
+# loraid=10
 # expdir="exp/unlearning_whp_llama3_8Bfull_MCQ_mcqmembothflatten_${setid}_mem1.0"
 expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}"
 
@@ -16,8 +16,11 @@ setname=new_mcq
 
 # python scripts/score_whp_mcq.py $expdir/${setname}_testoutput_${epoch}_${step}_mcq.json $setid
 
+for loratrainid in {10,,14}; do
 for setid in {1..5}; do
     # expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_lora_${loratrainid}"
     expdir="exp/unlearning_whp_llama3_8B_MCQ_mcq_${setid}_lora_${loratrainid}"
-    python scripts/score_whp_mcq.py $expdir/${setname}_testoutput_${epoch}_${step}_lora_${loraid}_mcq.json $setid
+    # python scripts/score_whp_mcq.py $expdir/${setname}_testoutput_${epoch}_${step}_lora_${loraid}_mcq.json $setid
+    python scripts/score_whp_mcq.py $expdir/${setname}_testoutput_${epoch}_${step}_lora_${loratrainid}_mcq.json $setid
+done
 done

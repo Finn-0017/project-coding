beeb6b039433feb7e97518dc02db40bbdcf49000
Finn <976195480@qq.com>
2026-03-13 09:18:43 +0000

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,7 +3,7 @@ export PYTHONPATH=$PWD
 mode="mcqmembothflatten"
 
 # setid=1
-loratrainid=12
+loratrainid=13
 
 modelpath=meta-llama/Llama-3.1-8B-Instruct
 traindata=./data/WHPplus/balanced_whp_mcq_train_dedup.json

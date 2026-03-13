e6d2b4787f021239ff16e710258bcf31d6032755
Finn <976195480@qq.com>
2026-03-13 12:13:32 +0000

    Update new_merge.sh

--- new_merge.sh
+++ new_merge.sh
@@ -9,17 +9,17 @@ nsamples=20
 
     python new_merge.py \
     --openend \
-    "exp/unlearning_whp_llama3_8B_WHP_whp_1_sample_${nsamples}_lora_${loraid}/forget_testoutput_1_final_lora_${loraid}.json" \
-    "exp/unlearning_whp_llama3_8B_WHP_whp_2_sample_${nsamples}_lora_${loraid}/forget_testoutput_1_final_lora_${loraid}.json" \
-    "exp/unlearning_whp_llama3_8B_WHP_whp_3_sample_${nsamples}_lora_${loraid}/forget_testoutput_1_final_lora_${loraid}.json" \
-    "exp/unlearning_whp_llama3_8B_WHP_whp_4_sample_${nsamples}_lora_${loraid}/forget_testoutput_1_final_lora_${loraid}.json" \
-    "exp/unlearning_whp_llama3_8B_WHP_whp_5_sample_${nsamples}_lora_${loraid}/forget_testoutput_1_final_lora_${loraid}.json" \
+    "exp/unlearning_whp_llama3_8B_WHP_whp_1_sample_${nsamples}_lora_${loratrainid}/forget_testoutput_1_final_lora_${loraid}.json" \
+    "exp/unlearning_whp_llama3_8B_WHP_whp_2_sample_${nsamples}_lora_${loratrainid}/forget_testoutput_1_final_lora_${loraid}.json" \
+    "exp/unlearning_whp_llama3_8B_WHP_whp_3_sample_${nsamples}_lora_${loratrainid}/forget_testoutput_1_final_lora_${loraid}.json" \
+    "exp/unlearning_whp_llama3_8B_WHP_whp_4_sample_${nsamples}_lora_${loratrainid}/forget_testoutput_1_final_lora_${loraid}.json" \
+    "exp/unlearning_whp_llama3_8B_WHP_whp_5_sample_${nsamples}_lora_${loratrainid}/forget_testoutput_1_final_lora_${loraid}.json" \
     --mcq \
-    "exp/unlearning_whp_llama3_8B_WHP_whp_1_sample_${nsamples}_lora_${loraid}/forget_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
-    "exp/unlearning_whp_llama3_8B_WHP_whp_2_sample_${nsamples}_lora_${loraid}/forget_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
-    "exp/unlearning_whp_llama3_8B_WHP_whp_3_sample_${nsamples}_lora_${loraid}/forget_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
-    "exp/unlearning_whp_llama3_8B_WHP_whp_4_sample_${nsamples}_lora_${loraid}/forget_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
-    "exp/unlearning_whp_llama3_8B_WHP_whp_5_sample_${nsamples}_lora_${loraid}/forget_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
+    "exp/unlearning_whp_llama3_8B_WHP_whp_1_sample_${nsamples}_lora_${loratrainid}/forget_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
+    "exp/unlearning_whp_llama3_8B_WHP_whp_2_sample_${nsamples}_lora_${loratrainid}/forget_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
+    "exp/unlearning_whp_llama3_8B_WHP_whp_3_sample_${nsamples}_lora_${loratrainid}/forget_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
+    "exp/unlearning_whp_llama3_8B_WHP_whp_4_sample_${nsamples}_lora_${loratrainid}/forget_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
+    "exp/unlearning_whp_llama3_8B_WHP_whp_5_sample_${nsamples}_lora_${loratrainid}/forget_mcq_testoutput_1_final_lora_${loraid}_mcq.json" \
     --output "temp_data/whp_lora_${loraid}_sample_${nsamples}.json"
 
     # python new_merge.py \

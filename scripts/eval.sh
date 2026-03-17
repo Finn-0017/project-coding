f631dfab582b0c030f2f5de7a9d64920f04eac49
Xingyu Yan <xy319@gpu-r-4.data.cluster>
2026-03-17 00:14:05 +0000

    data

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,10 +1,10 @@
 export PYTHONPATH=$PWD
 
-# setid=1
-loratrainid=10
+setid=5
+loratrainid=11
 loraid=$loratrainid
 nsample=20
-seed=2
+# seed=5
 
 epoch=1
 step=final
@@ -19,7 +19,8 @@ step=final
 # for loraid in {10..14}; do
 # for nsample in 5 10 20 50 100; do
 for setname in forget forget_mcq; do
-for setid in {1..5}; do
+# for setid in {1..5}; do
+for seed in {2..5}; do
     # expdir="exp/unlearning_whp_llama3_8B_WHP_whp_${setid}_sample_${nsample}_lora_${loratrainid}_seed_${seed}"
     expdir="exp/unlearning_whp_llama3_8B_MCQ_mcq_${setid}_lora_${loratrainid}_seed_${seed}"
 
